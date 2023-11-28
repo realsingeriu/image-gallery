d3dff2d75131d2b75995a2efe4736fc8d88de74c
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-28 14:19:34 +0900

    toggle 흑,백 모드 추가

--- src/ThemeToggle.jsx
+++ src/ThemeToggle.jsx
@@ -1,17 +1,17 @@
-import { useGlobalContext } from './context';
-import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
+import { useGlobalContext } from "./context";
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
 const ThemeToggle = () => {
-	const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
-	return (
-		<section className='toggle-container'>
-			<button className='dark-toggle' onClick={?}>
-				{isDarkTheme ? (
-					<BsFillMoonFill className='toggle-icon' />
-				) : (
-					<BsFillSunFill className='toggle-icon' />
-				)}
-			</button>
-		</section>
-	);
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+  return (
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <BsFillMoonFill style={{ color: "white" }} className="toggle-icon" />
+        ) : (
+          <BsFillSunFill className="toggle-icon" />
+        )}
+      </button>
+    </section>
+  );
 };
 export default ThemeToggle;
