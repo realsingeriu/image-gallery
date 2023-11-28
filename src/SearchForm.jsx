c1fed95ca227e808e92aded6569f638dccb9def3
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-28 14:57:09 +0900

    searchform css 완료

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { useGlobalContext } from "./context";
 
 const SearchForm = () => {
-  return <div></div>;
+  const { setSearchTerm } = useGlobalContext();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    setSearchTerm(searchValue);
+  };
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="search-input"
+          name="search"
+          placeholder="검색할 단어 입력해주세요."
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
-
 export default SearchForm;
